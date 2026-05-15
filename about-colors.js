1625d02ba2e0a04a3c1b9e3530b13dbf2a30703d
Joshua Job <joshuasjob@hotmail.com>
2026-05-15 11:49:22 -0600

    Desktop about me effect is satisfying instead of annoying

--- about-colors.js
+++ about-colors.js
@@ -1,23 +1,34 @@
 (function () {
   const states = [
-    ['taupe', 'yellow', 'cyan'],
-    ['cyan', 'taupe', 'yellow'],
-    ['yellow', 'cyan', 'taupe'],
+    ["taupe", "yellow", "cyan"],
+    ["cyan", "taupe", "yellow"],
+    ["yellow", "cyan", "taupe"],
   ];
   let current = 0;
-  const boxes = document.querySelectorAll('.about-callout');
-  const colorClasses = states[0].map(c => 'about-callout--' + c);
+  const boxes = document.querySelectorAll(".about-callout");
+  const colorClasses = states[0].map((c) => "about-callout--" + c);
 
-  function cycle() {
-    current = (current + 1) % 3;
+  function applyState(stateIndex) {
+    current = stateIndex;
     boxes.forEach((box, i) => {
       box.classList.remove(...colorClasses);
-      box.classList.add('about-callout--' + states[current][i]);
+      box.classList.add("about-callout--" + states[stateIndex][i]);
     });
   }
 
-  const isTouch = window.matchMedia('(hover: none) and (pointer: coarse)').matches;
-  boxes.forEach(box => {
-    box.addEventListener(isTouch ? 'click' : 'mouseenter', cycle);
-  });
+  function cycle() {
+    applyState((current + 1) % 3);
+  }
+
+  const isTouch = window.matchMedia(
+    "(hover: none) and (pointer: coarse)",
+  ).matches;
+
+  if (isTouch) {
+    boxes.forEach((box) => box.addEventListener("click", cycle));
+  } else {
+    boxes.forEach((box, i) => {
+      box.addEventListener("mouseenter", () => applyState((i + 1) % 3));
+    });
+  }
 })();
